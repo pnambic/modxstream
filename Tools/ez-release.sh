07b70753266da7e4f072ec6cabb2f3ee41830cc0
Lee Carver <leeca@pnambic.com>
2025-02-04 18:49:42 -0600

    Pull release process together

---
+++ Tools/ez-release.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+# Trigger a release via GitHub workflow on 'release/' tags
+set -o nounset
+set -o errexit
+
+readonly DIRNAME=$(dirname "$0")
+readonly SELF=$(cd "${DIRNAME}" && pwd)
+readonly RELPATH='releases'
+
+function releaseAndPush {
+  local tagName="$1"
+  local tagMsg="$2"
+
+  git tag -m "${tagMsg}" "${tagName}"
+  git push origin tag "${tagName}"
+}
+
+function usage {
+  cat >&2 <<HELP
+Usage: $(basename "$0") tagLabel tagMsg
+  Create a release tag and push it to GitHub to trigger a release
+  Both the tag label and tagMsg are expected
+  The prefix \'release/\' is automatically prepended to the tagLabel
+HELP
+}
+
+function die {
+  echo "$1" >&2
+  usage
+  exit 1
+}
+
+function main {
+
+  if [ $# -ne 2 ]; then
+    die "Release requires a label and a message"
+  fi
+
+  releaseAndPush "${RELPATH}/${1}" "${2}"
+}
+
+main "$@"
